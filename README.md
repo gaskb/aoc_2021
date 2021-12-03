7af54c76e8ec3a1247bb24fa242e657a60b6a0c2
Gas <gasparri@dudat.it>
2021-12-03 19:24:54 +0100

    day 3

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
 * simple check between 2 numbers
 * simple check between 2 numbers (generated manipulating array)
 
-
 ## Day 2 
 * simple xy movements
 * simple xy(z) movements
+
+## Day 2 
+* check binary data
+* little more complex check binary data
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import './appenv';
-import AOC from './02/main';
+import AOC from './03/main';
 
 async function startAoc() {
   const aoc = new AOC();
-  await aoc.part1('input/02/input_01.txt');
-  await aoc.part2('input/02/input_01.txt');
+  await aoc.part1('input/03/input_01.txt');
+  await aoc.part2('input/03/input_01.txt');
 }
 
 startAoc();
