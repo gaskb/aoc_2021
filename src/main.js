365696bbb1ed05a85f69e0ded25ba732e229a47d
Gas <gasparri@dudat.it>
2021-12-08 09:56:33 +0100

    Day 8

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import './appenv';
-import AOC from './07/main';
+import AOC from './08/main';
 
 async function startAoc() {
   const aoc = new AOC();
-  await aoc.part1('input/07/input_01.txt');
-  await aoc.part2('input/07/input_01.txt');
+  await aoc.part1('input/08/input_01.txt');
+  await aoc.part2('input/08/input_01.txt');
 }
 
 startAoc();
