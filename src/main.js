e13fb2a4080fa89799d08c0eb6ee9b77cef55c51
Gas <gasparri@dudat.it>
2021-12-14 22:04:12 +0100

    Day 13

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import './appenv';
-import AOC from './11/main';
+import AOC from './13/main';
 
 async function startAoc() {
   const aoc = new AOC();
-  await aoc.part1('input/11/input_01.txt');
-  await aoc.part2('input/11/input_01.txt');
+  await aoc.part1('input/13/input_01.txt');
+  await aoc.part2('input/13/input_01.txt');
 }
 
 startAoc();
